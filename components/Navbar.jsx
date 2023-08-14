a2640845c5dcbe68cffd97d87fe3cb6d8ba25eb0
SamikGarg <samik.garg.zebra@gmail.com>
2023-08-14 18:54:38 +0530

    nav updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,21 +13,23 @@ export const Navbar = ({ page = 0 }) => {
                     <Image src="/logo-text.png" alt="logo" width={400} height={70} />
                 </Link>
             </div>
-            <div>
+
+            <div className={stylesnav.links}>
                 <Link href="/" className={page == 0 ? stylesnav.active : stylesnav.navlink}>Home</Link>
                 <Link href="/apps" className={page == 1 ? stylesnav.active : stylesnav.navlink}>Apps</Link>
                 <Link href="/blogs" className={page == 4 ? stylesnav.active : stylesnav.navlink}>Blogs</Link>
                 <Link href="/about-us" className={page == 2 ? stylesnav.active : stylesnav.navlink}>About Us</Link>
                 <Link href="/contact-us" className={page == 3 ? stylesnav.active : stylesnav.navlink}>Contact Us</Link>
             </div>
+
             <div className={stylesnav.hamburger} onClick={() => setShowMobileNav(!showMobileNav)}>
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
-            
+
             {showMobileNav && 
-                <div className={stylesnav.mobile-nav}>
+                <div className={stylesnav.mobileNav}>
                     <Link href="/" className={page == 0 ? stylesnav.active : stylesnav.navlink}>Home</Link>
                     <Link href="/apps" className={page == 1 ? stylesnav.active : stylesnav.navlink}>Apps</Link>
                     <Link href="/blogs" className={page == 4 ? stylesnav.active : stylesnav.navlink}>Blogs</Link>
